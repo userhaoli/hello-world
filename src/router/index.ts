1bc08ca0260a7fed355d9421e2c97a27b665fa5c
-lihao <1911077369@qq.com>
2019-12-19 23:09:23 +0800

    测试

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,22 +1,37 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
-
+import Home from '../views/home/Home.vue'
+const Login = () => import(/* webpackChunkName: "about" */ '../views/login/Login.vue');
+const Register = () => import ('../views/reg/Register.vue')
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
     name: 'home',
-    component: Home
+    component: Home,
+    meta:{
+      requireLogin:true,
+      requireCache:true
+    }
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/login',
+    name: 'login',
+    component: Login,
+    meta:{
+      requireLogin:false,
+      requireCache:false
+    }
+  },
+  {
+    path: '/register',
+    name: 'register',
+    component: Register,
+    meta:{
+      requireLogin:false,
+      requireCache:false
+    }
   }
 ]
 
